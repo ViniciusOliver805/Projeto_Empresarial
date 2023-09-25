6e474e2751f13fc35708fa415631f492513fc1e0
Vinicius Oliver <viniciusoliver805@gmail.com>
2023-09-25 17:10:40 -0400

    começo do projeto, cadastro de usuario

---
+++ frontend/interface/src/App.jsx
@@ -0,0 +1,12 @@
+import Login from './components/UserRegistration/login_user/Classe_Login.jsx';
+
+function App() {
+  return (
+    <div className="App">
+       <Login />
+       <h1>AQUI É App</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/interface/src/components/UserRegistration/login_user/Classe_Login.jsx
@@ -0,0 +1,10 @@
+import { Title } from "./style.jsx"
+
+
+function Login() {
+  return (
+      <Title>Aqui é o title do logn</Title>
+  )
+}
+
+export default Login
